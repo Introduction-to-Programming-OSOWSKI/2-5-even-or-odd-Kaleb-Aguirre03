157cfdf7c4fb529b892a3edc35d76dee64047ec2
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-14 09:15:14 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+#use an if statement to determine if a number is even
+def isEven(n):
+    if n % 2 == 0:
+        return True
+
+    else:
+        return False
+
+#Test the function
+print(isEven(126))
